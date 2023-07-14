635bd419a0ff367ae532bdf971f24bc43ad638b4
kadence <kuriouskadence@gmail.com>
2023-07-14 07:19:43 -0700

    Initial Commit

---
+++ project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by kadence  on 7/14/23.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
